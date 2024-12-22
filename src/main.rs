ee32c7b36087cb92be61c7dce19d25cee11ad0a9
Johannes Horner <lzy.info@pm.me>
2024-12-22 19:31:06 +0100

    basic get/set

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 #![feature(os_str_display)]
 
-use anyhow::Context;
-use clap::{Parser, Subcommand};
+use anyhow::{Context, bail};
+use clap::{Parser, Subcommand, ValueEnum};
 use serde::Serialize;
-use std::{ffi::OsStr, fmt::Display};
+use std::{fmt::Display, str::FromStr};
 use udev::Enumerator;
+
+#[derive(Debug, Clone, ValueEnum)]
+enum PollRate {
+    #[value(name = "125")]
+    OneTwentyFive,
+    #[value(name = "500")]
+    FiveHundred,
+    #[value(name = "1000")]
+    OneThousand,
+}
+
+impl PollRate {
+    fn as_u32(&self) -> u32 {
+        match self {
+            PollRate::OneTwentyFive => 125,
+            PollRate::FiveHundred => 500,
+            PollRate::OneThousand => 1000,
+        }
+    }
+}
+
+impl FromStr for PollRate {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "125" => PollRate::OneTwentyFive,
+            "500" => PollRate::FiveHundred,
+            "1000" => PollRate::OneThousand,
+            _ => bail!("poll-rate needs to be 125, 500 or 1000"),
+        })
+    }
+}
 
 #[derive(Serialize)]
 struct Dpi {
@@ -13,9 +46,33 @@ struct Dpi {
 }
 
 #[derive(Serialize)]
-struct QueryResult {
+struct All {
     charge_level: Option<u8>,
     dpi: Option<Dpi>,
+    poll_rate: Option<u32>,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum GetAttribute {
+    ChargeLevel,
+    Dpi,
+    PollRate,
+}
+
+impl GetAttribute {
+    fn all(device: &Device) -> All {
+        All {
+            charge_level: device.charge_level(),
+            dpi: device.dpi(),
+            poll_rate: device.poll_rate().map(|p| p.as_u32()),
+        }
+    }
+}
+
+#[derive(Debug, Subcommand)]
+enum SetAttribute {
+    Dpi { x: u32, y: u32 },
+    PollRate { poll_rate: PollRate },
 }
 
 struct Device(udev::Device);
@@ -71,6 +128,23 @@ impl Device {
             }
         })
     }
+
+    fn poll_rate(&self) -> Option<PollRate> {
+        self.0
+            .attribute_value("poll_rate")
+            .map(|s| s.to_str().expect("utf8").parse::<PollRate>().unwrap())
+    }
+
+    fn set_dpi(&self, dpi: &Dpi) -> anyhow::Result<()> {
+        // self.0.set_attribute_value("dpi", todo!())?;
+        Ok(())
+    }
+
+    fn set_poll_rate(&mut self, poll_rate: PollRate) -> anyhow::Result<()> {
+        self.0
+            .set_attribute_value("poll_rate", poll_rate.as_u32().to_string())?;
+        Ok(())
+    }
 }
 
 impl Display for Device {
@@ -97,7 +171,15 @@ impl Display for Device {
 #[derive(Debug, Subcommand)]
 enum Command {
     List,
-    Query { device_serial: String },
+    Get {
+        device_serial: String,
+        attribute: Option<GetAttribute>,
+    },
+    Set {
+        device_serial: String,
+        #[command(subcommand)]
+        attribute: SetAttribute,
+    },
 }
 
 #[derive(Debug, Parser)]
@@ -116,13 +198,33 @@ fn main() -> anyhow::Result<()> {
                 eprintln!("{}:\n{device}\n", i + 1);
             }
         }
-        Command::Query { device_serial } => {
+        Command::Get {
+            device_serial,
+            attribute,
+        } => {
             let device = Device::from_device_serial(&device_serial)?;
-            let query_result = QueryResult {
-                charge_level: device.charge_level(),
-                dpi: device.dpi(),
-            };
-            eprintln!("{}", serde_json::to_string(&query_result)?);
+            if let Some(attribute) = attribute {
+                let json = match attribute {
+                    GetAttribute::ChargeLevel => serde_json::to_string(&device.charge_level())?,
+                    GetAttribute::Dpi => serde_json::to_string(&device.dpi())?,
+                    GetAttribute::PollRate => {
+                        serde_json::to_string(&device.poll_rate().map(|p| p.as_u32()))?
+                    }
+                };
+                eprintln!("{json:?}");
+            } else {
+                eprintln!("{}", serde_json::to_string(&GetAttribute::all(&device))?);
+            }
+        }
+        Command::Set {
+            device_serial,
+            attribute,
+        } => {
+            let mut device = Device::from_device_serial(&device_serial)?;
+            match attribute {
+                SetAttribute::Dpi { x, y } => todo!(),
+                SetAttribute::PollRate { poll_rate } => device.set_poll_rate(poll_rate)?,
+            }
         }
     };
 
